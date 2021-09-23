8f4378f8f327b9e10aa0b514f27b7ecad282991a
Wouter Schreurs <wouter@etrias.nl>
2021-09-23 14:03:44 +0200

    add quantities

--- src/Model/OrderItemTrait.php
+++ src/Model/OrderItemTrait.php
@@ -8,6 +8,8 @@ trait OrderItemTrait
 {
     protected ?string $id = null;
     protected ?int $quantity = null;
+    protected ?int $quantityShipped = null;
+    protected ?int $quantityCancelled = null;
 
     public function getId(): ?string
     {
@@ -29,6 +31,30 @@ trait OrderItemTrait
     public function setQuantity(?int $quantity): self
     {
         $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getQuantityShipped(): ?int
+    {
+        return $this->quantityShipped;
+    }
+
+    public function setQuantityShipped(?int $quantityShipped): self
+    {
+        $this->quantityShipped = $quantityShipped;
+
+        return $this;
+    }
+
+    public function getQuantityCancelled(): ?int
+    {
+        return $this->quantityCancelled;
+    }
+
+    public function setQuantityCancelled(?int $quantityCancelled): self
+    {
+        $this->quantityCancelled = $quantityCancelled;
 
         return $this;
     }

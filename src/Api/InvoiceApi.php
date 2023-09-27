958a91222be2f64625e77a3b448234dc6e2c7f85
Wouter Schreurs <wouter@etrias.nl>
2023-09-27 11:17:36 +0200

    invoice api

---
+++ src/Api/InvoiceApi.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Etrias\BolComConnector\Api;
+
+class InvoiceApi extends AbstractApi
+{
+    public function all(\DateTime $fromDate, \DateTime $untilDate): string
+    {
+        $uri = $this->createUri('/invoices', [], [
+            'period-start-date' => $fromDate->format('Y-m-d'),
+            'period-end-date' => $untilDate->format('Y-m-d'),
+        ]);
+
+        $response = $this->getJson($uri);
+
+        return $response->getBody()->getContents();
+    }
+
+    public function getSpecification(string $invoiceId): string
+    {
+        $uri = $this->createUri('/invoices/{invoice-id}/specification/', [
+            'invoice-id' => $invoiceId,
+        ]);
+
+        $response = $this->client->get($uri, [
+            'Content-Type' => 'application/vnd.retailer.v9+openxmlformats-officedocument.spreadsheetml.sheet',
+            'Accept' => 'application/vnd.retailer.v9+openxmlformats-officedocument.spreadsheetml.sheet',
+        ]);
+
+        return $response->getBody()->getContents();
+    }
+}
